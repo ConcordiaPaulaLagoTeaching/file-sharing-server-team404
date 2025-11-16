1f5d12564257552d1be17dd96d80a47fac7ab8e0
Gloria T <gloriaateta@gmail.com>
2025-11-16 06:00:17 -0500

    Implemented the multi-threading (many clients) and modified the filesystemserver file

--- FileServer/src/main/java/ca/concordia/server/FileServer.java
+++ FileServer/src/main/java/ca/concordia/server/FileServer.java
@@ -1,14 +1,11 @@
 package ca.concordia.server;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import ca.concordia.filesystem.FileSystemManager;
 
+// Server class
 public class FileServer {
-
     private FileSystemManager fsManager;
     private int port;
     public FileServer(int port, String fileSystemName, int totalSize){
@@ -27,75 +24,80 @@ public class FileServer {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Handling client: " + clientSocket);
-                try (
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
-                ) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        System.out.println("Received from client: " + line);
 
-                        // Split the line into three (Command, filename, content)
-                        String[] parts = line.split(" ", 3);
-                        String command = parts[0].toUpperCase();
+                ClientHandling cHandling = new ClientHandling(clientSocket, fsManager);
+                Thread thread = new Thread (cHandling);
+                thread.start();
 
-                            try {
-                                switch (command) {
-                                    case "CREATE":
-                                        fsManager.createFile(parts[1]);
-                                        writer.println("SUCCESS: File '" + parts[1] + "' created.");
-                                        break;
+                // try (
+                //         BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                //         PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
+                // ) {
+                //     String line;
+                //     while ((line = reader.readLine()) != null) {
+                //         System.out.println("Received from client: " + line);
 
-                                    case "WRITE":
-                                        fsManager.writeFile(parts[1], parts[2].getBytes());
-                                        writer.println("SUCCESS: File '" + parts[1] + "' written to.");
-                                        break;
+                //         // Split the line into three (Command, filename, content)
+                //         String[] parts = line.split(" ", 3);
+                //         String command = parts[0].toUpperCase();
 
-                                    case "READ":
-                                        byte[] content = fsManager.readFile(parts[1]);
-                                        writer.println("CONTENT: " + new String(content) + " (" + content.length + " bytes)");
-                                    break;
+                //             try {
+                //                 switch (command) {
+                //                     case "CREATE":
+                //                         fsManager.createFile(parts[1]);
+                //                         writer.println("SUCCESS: File '" + parts[1] + "' created.");
+                //                         break;
+
+                //                     case "WRITE":
+                //                         fsManager.writeFile(parts[1], parts[2].getBytes());
+                //                         writer.println("SUCCESS: File '" + parts[1] + "' written to.");
+                //                         break;
+
+                //                     case "READ":
+                //                         byte[] content = fsManager.readFile(parts[1]);
+                //                         writer.println("CONTENT: " + new String(content) + " (" + content.length + " bytes)");
+                //                     break;
                                     
-                                    case "LIST":
-                                        // writer.println("Checking for available files...");
-                                        String [] filesAvailable = fsManager.listFiles();
-                                        if (filesAvailable == null || filesAvailable.length == 0){
-                                             writer.println("No files found.");
-                                        }else{
-                                            writer.println("Current files available: " + String.join(", ",filesAvailable));
-                                        }
-                                    break;
+                //                     case "LIST":
+                //                         // writer.println("Checking for available files...");
+                //                         String [] filesAvailable = fsManager.listFiles();
+                //                         if (filesAvailable == null || filesAvailable.length == 0){
+                //                              writer.println("No files found.");
+                //                         }else{
+                //                             writer.println("Current files available: " + String.join(", ",filesAvailable));
+                //                         }
+                //                     break;
 
-                                    case "DELETE":
-                                        fsManager.deleteFile(parts[1]);
-                                        writer.println("SUCCESS: File " + parts[1]+ " deleted");
-                                        break;
+                //                     case "DELETE":
+                //                         fsManager.deleteFile(parts[1]);
+                //                         writer.println("SUCCESS: File " + parts[1]+ " deleted");
+                //                         break;
 
-                                    case "QUIT":
-                                        writer.println("SUCCESS: Disconnecting.");
-                                        return;
+                //                     case "QUIT":
+                //                         writer.println("SUCCESS: Disconnecting.");
+                //                         return;
 
-                                    default:
-                                        writer.println("ERROR: Unknown command.");
-                                        break;
-                                }
-                            } catch (Exception e) {
-                                // Catch any error thrown by FileSystemManager and send it back
-                                e.printStackTrace();
-                                writer.println(e.getMessage());
-                            }
+                //                     default:
+                //                         writer.println("ERROR: Unknown command.");
+                //                         break;
+                //                 }
+                //             } catch (Exception e) {
+                //                 // Catch any error thrown by FileSystemManager and send it back
+                //                 e.printStackTrace();
+                //                 writer.println(e.getMessage());
+                //             }
 
-                            writer.flush();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        clientSocket.close();
-                    } catch (Exception e) {
-                        // Ignore
-                    }
-                }
+                //             writer.flush();
+                //     }
+                // } catch (Exception e) {
+                //     e.printStackTrace();
+                // } finally {
+                //     try {
+                //         clientSocket.close();
+                //     } catch (Exception e) {
+                //         // Ignore
+                //     }
+                // }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,3 +106,100 @@ public class FileServer {
     }
 
 }
+
+
+// class ClientHandling implements Runnable{
+
+//     private Socket clientSocket;
+//     private FileSystemManager fsManager;
+
+//     //constructor
+//     public ClientHandling (Socket cSocket, FileSystemManager fsManager){
+//         this.clientSocket = cSocket;
+//         this.fsManager = fsManager;
+//     }
+
+//     @Override
+//     public void run(){
+//         System.out.println("running..." + clientSocket);
+
+//         //implement the input and output buffer
+//         // wait for input from client and send response back to client (show relations in UML diagram)
+//         //close all streams and sockets
+
+//         //try catch for reader and writer
+//         try (
+//             BufferedReader reader = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));
+//             PrintWriter writer = new PrintWriter (clientSocket.getOutputStream(), true);
+//         )
+//          {
+//             String request;
+
+//             //reads request from client 
+//             while((request = reader.readLine()) != null){
+
+//                 // Split the line into three (Command, filename, content)
+//                 String[] parts = request.split(" ", 3);
+//                 String command = parts[0].toUpperCase();
+
+
+//                 try {
+//                     switch (command) {
+//                         case "CREATE":
+//                             fsManager.createFile(parts[1]);
+//                             writer.println("SUCCESS: File '" + parts[1] + "' created.");
+//                             break;
+
+//                         case "WRITE":
+//                             fsManager.writeFile(parts[1], parts[2].getBytes());
+//                             writer.println("SUCCESS: File '" + parts[1] + "' written to.");
+//                             break;
+
+//                         case "READ":
+//                             byte[] content = fsManager.readFile(parts[1]);
+//                             writer.println("CONTENT: " + new String(content) + " (" + content.length + " bytes)");
+//                         break;
+                        
+//                         case "LIST":
+//                             // writer.println("Checking for available files...");
+//                             String [] filesAvailable = fsManager.listFiles();
+//                             if (filesAvailable == null || filesAvailable.length == 0){
+//                                     writer.println("No files found.");
+//                             }else{
+//                                 writer.println("Current files available: " + String.join(", ",filesAvailable));
+//                             }
+//                         break;
+
+//                         case "DELETE":
+//                             fsManager.deleteFile(parts[1]);
+//                             writer.println("SUCCESS: File " + parts[1]+ " deleted");
+//                             break;
+
+//                         case "QUIT":
+//                             writer.println("SUCCESS: Disconnecting.");
+//                             return;
+
+//                         default:
+//                             writer.println("ERROR: Unknown command.");
+//                             break;
+//                     }
+//                 } catch (Exception e) {
+//                     writer.println("ERROR: " + e.getMessage());
+//                 } 
+//             }
+            
+//         } catch (Exception e) {
+//             System.out.println("Completed..." + e.getMessage());
+
+//         }
+
+
+
+//     }
+
+//     // public static void main(String[] args) {
+//     //     FileHandler filehandler = new FileHandler();
+//     //     Thread thread = new Thread (filehandler);
+//     //     thread.start();
+//     // }
+// }
